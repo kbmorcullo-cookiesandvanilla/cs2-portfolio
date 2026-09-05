0a36bf78f13bc59aad46c4427501abc453244fdb
kbmorcullo-cookiesandvanilla <kbmorcullo@brc.pshs.edu.ph>
2026-09-05 18:11:45 +0800

    Create score_checker.py

---
+++ q1/score_checker.py
@@ -0,0 +1,19 @@
+s=int(input("score:"))
+
+if s>=101:
+  print("Invalid Score") 
+  
+elif s<= -1:
+  print("Invalid Score")
+  
+elif  s>=90:
+  print("Outstanding")
+  
+elif s>=80:
+  print("Very Satisfactory")
+  
+elif s>=75:
+  print("Satisfactory")
+  
+else : 
+  print("Needs Improvement")
